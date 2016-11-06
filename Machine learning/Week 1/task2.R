b967f4579eca26826dfff7fb3e60bd7c70b16085
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-06 21:05:24 +0200

    Added MSE calculation for 1000 numbers

--- Machine learning/Week 1/task2.R
+++ Machine learning/Week 1/task2.R
@@ -1,3 +1,7 @@
+mse = function(points, func, numbers) {
+  return(sum((points - func) ^ 2) / numbers)
+} 
+
 set.seed(20)
 
 degree = 10
@@ -9,7 +13,7 @@ y = 2 + x - 0.5 * x ^ 2
 errors = rnorm(n = numbers, mean = 0, sd = 0.4)
 ywitherrors = y + errors
 
-plot(x, ywitherrors)
+plot(x, ywitherrors, pch=16)
 
 regression = lm(ywitherrors ~ poly(x, degree))
 values = seq(interval[1], interval[2], by = 0.1)
@@ -17,5 +21,21 @@ model = predict(regression, data.frame(x = values), interval = "confidence", lev
 
 lines(values, model[,1], col='green', lwd=5)
 
-mse = mean(regression$residuals^2)
-sprintf("MSE: %f", mse)
+mse_value = mean(regression$residuals^2)
+#mse_value = mse()
+sprintf("MSE: %f", mse_value)
+
+# task 2
+numbers = 1000
+x = runif(numbers, interval[1], interval[2])
+y = 2 + x - 0.5 * x ^ 2
+errors = rnorm(n = numbers, mean = 0, sd = 0.4)
+ywitherrors = y + errors
+
+points(x, y = ywitherrors, col="red")
+
+values = seq(interval[1], interval[2], length.out = numbers)
+newprediction = predict(regression, data.frame(x = values), interval = "confidence", level = 0.95)
+
+new_mse_value = mse(points = newprediction[,1], func = ywitherrors, numbers = numbers)
+sprintf("MSE: %f", new_mse_value)
