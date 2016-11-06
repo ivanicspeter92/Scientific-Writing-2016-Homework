ab3d5c3b03d89536e9ba7b3b6b3cb250a92d830f
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-06 17:48:09 +0200

    Completed task 8

--- Machine learning/Week 1/task8.R
+++ Machine learning/Week 1/task8.R
@@ -1,3 +1,36 @@
+plotOutstateVSPrivate = function(data) {
+  boxplot(data$Outstate, data$Private, names = c("Outstage", "Private"), log = "y")
+}
+
+plotOutstateVSElite = function(data) {
+  boxplot(data$Outstate, data$Elite, names = c("Outstage", "Elite"), log = "y")
+}
+
 library(ISLR)
 data("College")
 # college = csv.read("college.csv", header = TRUE, sep = ";")
+
+rownames(College) 
+#fix(College)
+
+# 8/ii
+summary(College)
+pairs(College[,1:10])
+
+# 8/iii
+plotOutstateVSPrivate(College)
+
+# 8/iv
+Elite=rep("No", nrow(College))
+Elite[College$Top10perc > 50] = "Yes"
+Elite = as.factor(Elite)
+College = data.frame(College, Elite)
+summary(College)
+plotOutstateVSElite(College)
+
+# 8/v
+par(mfrow=c(2,2))
+hist(x = College$Books)
+hist(x = College$Grad.Rate)
+hist(x = College$PhD)
+hist(x = College$Accept)
