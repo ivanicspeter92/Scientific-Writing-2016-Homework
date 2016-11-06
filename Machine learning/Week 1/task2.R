a8174c6b0e5c2ddb70846467bad9ea415396dc2a
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-06 19:35:28 +0200

    Added MSE calculation

--- Machine learning/Week 1/task2.R
+++ Machine learning/Week 1/task2.R
@@ -6,13 +6,16 @@ interval = c(-3, 3)
 
 x = runif(numbers, interval[1], interval[2])
 y = 2 + x - 0.5 * x ^ 2
-errors = rnorm(n = 30, mean = 0, sd = 0.4)
-y = y + errors
+errors = rnorm(n = numbers, mean = 0, sd = 0.4)
+ywitherrors = y + errors
 
-plot(x, y)
+plot(x, ywitherrors)
 
-regression = lm(y ~ poly(x, degree))
+regression = lm(ywitherrors ~ poly(x, degree))
 values = seq(interval[1], interval[2], by = 0.1)
 model = predict(regression, data.frame(x = values), interval = "confidence", level = 0.95)
 
 lines(values, model[,1], col='green', lwd=5)
+
+mse = mean(regression$residuals^2)
+sprintf("MSE: %f", mse)
