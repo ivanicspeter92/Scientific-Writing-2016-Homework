6eb950da6932c09a0fac9bf6f6d512ca95e97472
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-13 15:11:44 +0200

    Adde first implementation of K nearest neighbours algorithm

--- Location Awareness/Week 2/Radio map/k-nearest-neighbours.R
+++ Location Awareness/Week 2/Radio map/k-nearest-neighbours.R
@@ -1,2 +1,18 @@
-points = read.csv("radio_map.csv", header =TRUE, sep = ";")
+measurements = read.csv("radio_map.csv", header =TRUE, sep = ";")
+plot(x = measurements$X, y = measurements$Y)
 
+k = 5
+mySinglalStrength = c(-74, -80)
+
+differences = matrix(c(measurements$SS1 - mySinglalStrength[1], measurements$SS2 - mySinglalStrength[2]), ncol = 2)
+
+averageDifferences = matrix(data = c(1:10, rowMeans(differences)), ncol = 2)
+averageDifferences = averageDifferences[order(averageDifferences[,2]),] # sorting measurements by differences
+
+indexOfClosestKmeasurements = head(averageDifferences[,1], k)
+kNearestNeighbours = measurements[indexOfClosestKmeasurements[1:k],]
+
+points(x = kNearestNeighbours$X, y = kNearestNeighbours$Y, col = "green", pch = 16)
+
+myPosition = c(mean(kNearestNeighbours$X), mean(kNearestNeighbours$Y))
+points(x = myPosition[1], y = myPosition[2], col = "red", pch = 4)
