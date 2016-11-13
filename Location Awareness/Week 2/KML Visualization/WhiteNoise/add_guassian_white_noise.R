78900dfbc683bc2a0a8b3d9b98b2d503a17aef35
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-13 10:00:34 +0200

    Fixed variance

--- Location Awareness/Week 2/KML Visualization/WhiteNoise/add_guassian_white_noise.R
+++ Location Awareness/Week 2/KML Visualization/WhiteNoise/add_guassian_white_noise.R
@@ -3,10 +3,10 @@ coordinates = read.csv("tokyo-lon-lat.csv",header = FALSE, sep = ",")
 par(mfrow=c(1, 2))
 plot(coordinates, xlab = "Latitude", ylab = "Longitute")
 
-variance = 0.01
-noise = matrix(rnorm(n = 100, mean = 0, sd = variance), ncol = 2)
+variance = 0.075
+noise = matrix(rnorm(n = 100, mean = 0, sd = variance ^ 2), ncol = 2)
 
 newPoints = coordinates + noise 
 
 plot(coordinates, xlab = "Latitude", ylab = "Longitute")
-points(newPoints, col="red", )
+points(newPoints, col="red")
