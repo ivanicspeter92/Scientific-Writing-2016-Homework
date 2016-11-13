68e598708bd2da723aba0e2a52ae48541b798314
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-13 15:35:19 +0200

    Refactoring

--- Location Awareness/Week 2/Radio map/k-nearest-neighbours.R
+++ Location Awareness/Week 2/Radio map/k-nearest-neighbours.R
@@ -1,14 +1,20 @@
-estimatePosition = function(signalStrength, points, k = 1, weighted = FALSE, highlightNeighboursOnPlot = FALSE) {
+kNearestNeighbours = function(signalStrength, points, k = 1, weighted = FALSE) {
   differences = matrix(c(points$SS1 - signalStrength[1], points$SS2 - signalStrength[2]), ncol = 2)
   
   averageDifferences = matrix(data = c(1:10, rowMeans(differences)), ncol = 2)
   averageDifferences = averageDifferences[order(averageDifferences[,2]),] # sorting measurements by differences
   
   indexOfClosestKmeasurements = head(averageDifferences[,1], k)
-  kNearestNeighbours = points[indexOfClosestKmeasurements[1:k],]
+  result = points[indexOfClosestKmeasurements[1:k],]
+  
+  return(result)
+}
+
+estimatePosition = function(signalStrength, points, k = 1, weighted = FALSE, highlightNeighboursOnPlot = FALSE, neighbourColor = "green") {
+  kNearestNeighbours = kNearestNeighbours(signalStrength = signalStrength, points = points, k = k, weighted = weigthed)
   
   if (highlightNeighboursOnPlot) {
-    points(x = kNearestNeighbours$X, y = kNearestNeighbours$Y, col = "green", pch = 16)
+    points(x = kNearestNeighbours$X, y = kNearestNeighbours$Y, col = neighbourColor, pch = 16)
   }
   return(c(mean(kNearestNeighbours$X), mean(kNearestNeighbours$Y)))
 } 
@@ -16,8 +22,13 @@ estimatePosition = function(signalStrength, points, k = 1, weighted = FALSE, hig
 measurements = read.csv("radio_map.csv", header = TRUE, sep = ";")
 plot(x = measurements$X, y = measurements$Y)
 
-k = 5
+k = 3
 mySinglalStrength = c(-74, -80)
 
-myPosition = estimatePosition(signalStrength = mySinglalStrength, points = measurements, k = 3)
+myPosition = estimatePosition(signalStrength = mySinglalStrength, points = measurements, k = k, highlightNeighboursOnPlot = TRUE)
 points(x = myPosition[1], y = myPosition[2], col = "red", pch = 4)
+
+k = 4
+
+myPosition = estimatePosition(signalStrength = mySinglalStrength, points = measurements, k = k, weighted = TRUE)
+points(x = myPosition[1], y = myPosition[2], col = "blue", pch = 4)
