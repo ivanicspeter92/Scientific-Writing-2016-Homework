b1ab89ef753fc1df5d1e7becf60bf2903c912e19
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-16 22:38:44 +0200

    Refactoring

--- Machine learning/Week 2/problem4.R
+++ Machine learning/Week 2/problem4.R
@@ -1,6 +1,14 @@
 require(ISLR)
 data("Weekly")
 
+getConfusionMatrix = function(predictions) {
+  confusionMatrix = rep("No", length(predictions))
+  confusionMatrix[predictions > 0.5] = "Yes"
+  confusionMatrix = table(confusionMatrix)
+  
+  return(confusionMatrix)
+}
+
 # a)
 head(Weekly)
 summary(Weekly)
@@ -17,3 +25,8 @@ par(mfrow=c(1, 1))
 model = glm(Direction ~ Lag1 + Lag2 + Lag3 + Lag4 + Lag5 + Volume, data = Weekly, family = binomial)
 summary(model)
 coef(model)
+
+# c)
+predictions = predict(model, type="response")
+confusionMatrix = getConfusionMatrix(predictions)
+sprintf("%f of the observations are Yes, %f of the observations are No instances", confusionMatrix[2] / length(predictions) * 100,confusionMatrix[1] / length(predictions) * 100)
