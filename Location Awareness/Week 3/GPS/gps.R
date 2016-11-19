00210b94fb4848b328c0344ab9687a18fdf72dae
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-19 17:36:21 +0200

    Added B)

--- Location Awareness/Week 3/GPS/gps.R
+++ Location Awareness/Week 3/GPS/gps.R
@@ -1,4 +1,5 @@
 require(proxy)
+require(MASS)
 
 getDOPMatrix = function(receiver_coordinates, satellite_coordinates) {
   dop = c()
@@ -19,15 +20,24 @@ getDOPMatrix = function(receiver_coordinates, satellite_coordinates) {
   }
   
   dop = matrix(unlist(dop), ncol = 4, byrow = TRUE)
+  dop = ((t(dop) * dop) ^ -1) # inverse matrix Q incorrect
   return(dop)
 }
 
 ecef_coordinates = read.csv("ecef_coordinates.csv", header = FALSE, sep = ",")
 receiver_coordinates = ecef_coordinates[1, ]
-satellite_coordinates = ecef_coordinates[2:6, ]
+satellite_coordinates = ecef_coordinates[3:6, ]
 
 # a) 
 dopMatrix = getDOPMatrix(receiver_coordinates, satellite_coordinates)
+
+# b) 
+#gdop = sqrt(trace(dopMatrix))
+tdop = sqrt(dopMatrix[4,4])
+pdop = sqrt(dopMatrix[1,1] + dopMatrix[2, 2] + dopMatrix[3, 3])
+hdop = sqrt(dopMatrix[1,1] + dopMatrix[2, 2])
+vdop = sqrt(dopMatrix[3,3]) 
+
 
 particles = read.csv("particles.csv", header = TRUE, sep = ",")
 
