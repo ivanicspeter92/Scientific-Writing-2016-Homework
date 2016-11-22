813070c14f60278baa3b6b0db827cd6c60d2d20f
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-22 21:43:19 +0200

    Added solution

--- Location Awareness/Week 3/Particle Filter/particleFilter.R
+++ Location Awareness/Week 3/Particle Filter/particleFilter.R
@@ -1,4 +1,40 @@
 library(truncnorm)
 
+getNewParticles = function(particles, mean = 0.5, sd = 1.5) {
+  newparticles = c()
+  for (i in 1:nrow(particles)) {
+    distance = abs(rnorm(n = 1, mean = mean, sd = sd))
+    weight = getUpdatedWeigth(particles[i,], distance)
+    
+    newparticles = c(newparticles, weight, distance)
+  }
+  
+  newparticles = matrix(newparticles, ncol = 2, byrow = TRUE)
+  return(newparticles)
+}
+
+getUpdatedWeigth = function(particle, distance) {
+  return((distance * particle$Weight) / particle$Distance)
+}
+
+needsResampling = function(weights, treshold) {
+  if(treshold > sum(weights ^ 2)) {
+    return(FALSE)
+  }
+  return(TRUE)
+}
+
 particles = read.csv("particles.csv", header = TRUE, sep = ",")
-movements = rtruncnorm(n = nrow(particles), a = 0.5, b = 1.5)
+
+# a)
+newparticles = getNewParticles(particles)
+
+#b) 
+neffTreshold = 2
+"Needs resampling?"
+needsResampling(weights = newparticles[1,], treshold = neffTreshold)
+
+#c)
+newparticles = getNewParticles(particles, mean = 0.5, sd = 0.5)
+"Needs resampling?"
+needsResampling(weights = newparticles[1,], treshold = neffTreshold)
