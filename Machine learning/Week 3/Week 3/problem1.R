f9e4ddb28de31d4cb7206934b427a6af9e7b7a61
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-23 20:13:30 +0200

    Added implementation for a) and b) part

--- Machine learning/Week 3/Week 3/problem1.R
+++ Machine learning/Week 3/Week 3/problem1.R
@@ -9,7 +9,17 @@ getCorrelationCoefficient = function(xr, xs) {
 p = 2
 variances = c(2.0, 3.0)
 means = c(0, 0)
+covarianceMatrix = matrix(c(2, -1.83,-1.83, 3), nrow = 2)
 correlation_coefficient = -0.75
-n = 200
+n = 400
 
-x = mvrnorm(n = n, mu = means, Sigma = variances)
+x = mvrnorm(n = n, mu = means, Sigma = covarianceMatrix)
+plot(x)
+cor(x[,1], x[,2])
+cov(x[,1], x[,2])
+
+#b)
+dataDensity = kde2d(x[,1], x[,2])
+image(dataDensity)
+persp(dataDensity)
+contour(dataDensity)
