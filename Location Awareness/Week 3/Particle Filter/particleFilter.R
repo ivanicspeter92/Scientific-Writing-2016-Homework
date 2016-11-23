985daf29501fae91c795c7ec9e1fb2c7ce4b0087
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-23 14:10:48 +0200

    Refactoring

--- Location Awareness/Week 3/Particle Filter/particleFilter.R
+++ Location Awareness/Week 3/Particle Filter/particleFilter.R
@@ -3,38 +3,53 @@ library(truncnorm)
 getNewParticles = function(particles, mean = 0.5, sd = 1.5) {
   newparticles = c()
   for (i in 1:nrow(particles)) {
-    distance = abs(rnorm(n = 1, mean = mean, sd = sd))
-    weight = getUpdatedWeigth(particles[i,], distance)
-    
-    newparticles = c(newparticles, weight, distance)
+    weight = getUpdatedWeight(particles[i,], mean = mean, sd = sd)
+    newparticles = c(newparticles, weight, particles[i,]$Distance)
   }
   
   newparticles = matrix(newparticles, ncol = 2, byrow = TRUE)
   return(newparticles)
 }
 
-getUpdatedWeigth = function(particle, distance) {
-  return((distance * particle$Weight) / particle$Distance)
+getUpdatedWeight = function(particle, mean, sd) {
+  probabilty = dnorm(x = particle$Distance, mean = mean, sd = sd)
+  return(particle$Weight * probabilty)
+}
+
+normalizeWeights = function(weights) {
+  sum = sum(weights)
+  for (i in 1:length(weights)) {
+    weights[i] = weights[i] / sum
+  }
+  
+  return(weights)
 }
 
 needsResampling = function(weights, treshold) {
-  if(treshold > sum(weights ^ 2)) {
+  if(treshold < numberOfEffectiveParticles(weights)) {
     return(FALSE)
   }
   return(TRUE)
 }
 
+numberOfEffectiveParticles = function(weights) {
+  return(1 / sum(weights ^ 2))
+}
+
+
 particles = read.csv("particles.csv", header = TRUE, sep = ",")
 
 # a)
 newparticles = getNewParticles(particles)
+newparticles[,1] = normalizeWeights(newparticles[,1])
 
 #b) 
-neffTreshold = 2
+neffTreshold = 2.0
 "Needs resampling?"
-needsResampling(weights = newparticles[1,], treshold = neffTreshold)
+needsResampling(weights = newparticles[,1], treshold = neffTreshold)
 
 #c)
 newparticles = getNewParticles(particles, mean = 0.5, sd = 0.5)
+newparticles[,1] = normalizeWeights(newparticles[,1])
 "Needs resampling?"
-needsResampling(weights = newparticles[1,], treshold = neffTreshold)
+needsResampling(weights = newparticles[,1], treshold = neffTreshold)
