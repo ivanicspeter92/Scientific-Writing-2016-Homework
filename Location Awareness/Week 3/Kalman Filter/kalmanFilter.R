ca2a193b549c5778120766bd30e07c28ebf0d234
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-25 16:00:19 +0200

    Added initial solution to kalman filter

--- Location Awareness/Week 3/Kalman Filter/kalmanFilter.R
+++ Location Awareness/Week 3/Kalman Filter/kalmanFilter.R
@@ -1,6 +1,26 @@
 measurements = read.csv("noisyCompass.csv", header = FALSE)
-S = diag(x = 0.15, ncol = 3, nrow = 3)
+
+#configuration
 sigma = 1.25 * 10 ^ -4
 Q = sigma * diag(x = 1, ncol = 3, nrow = 3)
 A = diag(x = 1, ncol = 3, nrow = 3)
 R = 0.15 * diag(x = 1, ncol = 3, nrow = 3)
+
+# predict
+predict = function(measurements, A, Q) {
+  covarianceMatrix = cov(measurements) # P
+  means = colMeans(measurements) # m
+
+  mPredict = A %*% means
+  pPredict = A %*% (covarianceMatrix %*% t(A)) + Q
+  return(c(mPredict, pPredict))
+}
+#update
+update = function(mPredict, pPredict) {
+  covarianceResidual = solve(A %*% pPredict %*% t(A))
+  kalmanGain = pPredict %*% t(A) %*% covarianceResidual
+  trueM = mPredict + (kalmanGain %*% (measurements - (A %*% mPredict)))
+  trueP = pPredict - (kalmanGain %*% pPredict %*% t(A))
+  
+  return(c(trueM, trueP))
+}
