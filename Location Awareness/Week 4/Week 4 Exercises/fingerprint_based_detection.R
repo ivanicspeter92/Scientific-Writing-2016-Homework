501763abc68154602677c25279134ea1913753e7
Peter Ivanics <peter.ivanics@reslink.fi>
2016-11-26 20:24:59 +0200

    Corrected answer for second task, added answer to third task

--- Location Awareness/Week 4/Week 4 Exercises/fingerprint_based_detection.R
+++ Location Awareness/Week 4/Week 4 Exercises/fingerprint_based_detection.R
@@ -3,12 +3,34 @@ extendedTanimoto = function(A, B) {
   return(nominator / (sum(A ^ 2) + sum(B ^ 2) - nominator))
 }
 
-data = read.csv("rssVals.csv", header = FALSE, sep = ",")
-likelihoods = matrix(data = 0, nrow = ncol(data), ncol = ncol(data))
-
-for (i in 1:ncol(data)) {
-  for (j in 1:i) {
-    likelihoods[j,i] = extendedTanimoto(data[,i], data[,j])
+getLikelihoods = function(data) {
+  likelihoods = matrix(data = 0, nrow = ncol(data), ncol = ncol(data))
+  
+  for (i in 1:ncol(data)) {
+    for (j in 1:i) {
+      likelihoods[j,i] = extendedTanimoto(data[,i], data[,j])
+    }
   }
+  
+  likelihoods = round(likelihoods, 2)
+  return(likelihoods)
 }
-likelihoods = round(likelihoods, 2)
+
+getResponseRateVector = function(data) {
+  result = c()
+  j = 1
+  for(i in seq(from = 1, to = nrow(data), by = 2)) {
+    n = length(data[i,]) + length(data[i + 1,])
+    ni = length(data[i,][which(data[i,] != 0)]) + length(data[i + 1,][which(data[i + 1,] != 0)])
+    
+    result = c(result, ni / n)
+  }
+  return(as.vector(result))
+}
+
+# a)
+data = read.csv("rssVals.csv", header = FALSE, sep = ",")
+likelihoods = getLikelihoods(data)
+
+# b) 
+responseRateVector = getResponseRateVector(data)
