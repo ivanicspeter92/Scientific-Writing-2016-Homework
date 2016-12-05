a0957af0e4bd09751e77e2c6c99386c1126e7699
Peter Ivanics <peter.ivanics@reslink.fi>
2016-12-05 22:43:30 +0200

    Added b)

--- Location Awareness/Week 5/Week 5 exercises/transportation_modes.R
+++ Location Awareness/Week 5/Week 5 exercises/transportation_modes.R
@@ -24,16 +24,31 @@ for (i in 1:(nrow(data) - 1)) {
   }
 }
 
-pc
-ps
-pv
-
 totalDistance = sum(data$Distance)
 
 hcr = pc / (totalDistance / unitDistance)
 sr = ps / (totalDistance / unitDistance)
 vcr = pv / (totalDistance / unitDistance)
 
-hcr
-sr
-vcr
+#b)
+mode1 = read.csv("mode1.csv", header = FALSE)
+mode2 = read.csv("mode2.csv", header = FALSE)
+
+means1 = colMeans(mode1)
+means2 = colMeans(mode2)
+
+strongestAccessPoint1 = as.numeric(which.max(means1))
+strongestAccessPoint2 = as.numeric(which.max(means2))
+
+sigma1 = var(mode1[, strongestAccessPoint1])
+sigma2 = var(mode2[, strongestAccessPoint2])
+
+intensities1 = colSums(mode1)
+variances1 = apply(mode1, 2, var)
+mean(intensities1)
+mean(variances1)
+
+intensities2 = colSums(mode2)
+variances2 = apply(mode2, 2, var)
+mean(intensities2)
+mean(variances2)
