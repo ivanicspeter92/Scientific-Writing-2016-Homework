b580ffb79d7b52ae21ba470c8780568c582b26f9
Peter Ivanics <peter.ivanics@reslink.fi>
2016-12-07 13:07:29 +0200

    Added implementation for trace.R

--- Location Awareness/Week 5/Week 5 exercises/trace.R
+++ Location Awareness/Week 5/Week 5 exercises/trace.R
@@ -1,1 +1,29 @@
+calculateDifferences =  function(shortestPaths) {
+  diff = c()
+  for(i in 1:(nrow(shortestPaths) - 1)) {
+    
+    diffX = shortestPaths[i,]$V1 - shortestPaths[i + 1,]$V1
+    diffY = shortestPaths[i,]$V2 - shortestPaths[i + 1,]$V2
+    
+    value = atan(diffX / diffY)
+    if (!is.na(value))
+      diff = c(diff, value)
+  }
+  
+  return(round(diff,2))
+}
+
+countSegments = function(differences) {
+  count = 0
+  
+  for (i in 1:(length(differences) - 1)) {
+    if(differences[i] == differences[i + 1]) {
+      count = count + 1
+    }
+  }
+  return(count)
+}
+
 shortestPaths = read.csv("trace.csv", header = FALSE, sep = ",")
+diff = calculateDifferences(shortestPaths)
+segments = countSegments(diff)
