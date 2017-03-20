3e3785c4d555109e1e5da3b1956b5961c96f6814
Peter Ivanics <peter@choicely.com>
2017-03-20 22:12:01 +0200

    Added solution to Eigen value decomposition

--- Advanced course in Machine Learning/AML-Week1/Eigen-value_decomposition.R
+++ Advanced course in Machine Learning/AML-Week1/Eigen-value_decomposition.R
@@ -1,18 +1,34 @@
-data = read.csv("ex_1_data.csv", header = F)
+reconstructionError = function(data, L, plot = FALSE) {
+  X = cov(data)
+  eigen = eigen(X)
+  
+  W = eigen$vectors[1:L,]
+  reduced_data = as.matrix(data) %*% t(W)
+  
+  if(plot) {
+    plot(reduced_data, xlab="X", ylab="Y", pch = 4)
+    title(main = "The dataset projected to the first L=2 eigenvectors", line = 3)
+  }
+  
+  backwardProjection = reduced_data %*% W 
+  error = sum((data - backwardProjection)^2)
+  
+  return(error)
+}
 
+data = read.csv("ex_1_data.csv", header = F)
 sprintf("D=%d, N=%d", nrow(data), ncol(data))
 
 L = 2
-X = cov(data)
-eigen = eigen(X)
+reconstructionError(data = data, L = L, plot = TRUE)
 
-eigen$values
-eigen$vectors
+reconstructionErrors = c()
 
-W = t(eigen$vectors[1:L,])
-reduced_data =  as.matrix(data) %*% W
+for (i in L:ncol(data)) {
+  reconstructionErrors = c(reconstructionErrors, reconstructionError(data = data, L = i))
+}
 
-# plot(data)
-# title(main = "The original dataset", xlab="", ylab="", line = 3)
-plot(reduced_data, xlab="X", ylab="Y")
-title(main = "The dataset projected to the first L=2 eigenvectors", line = 3)
+plot(x = L:ncol(data), y = reconstructionErrors, type = "line", xlab="L", ylab="Reconstruction error", xaxt = "n")
+points(x = L:ncol(data), y = reconstructionErrors)
+axis(side = 1, at = c(1:5))
+title(main = "The reconstruction error in terms of reduced dimensions", line = 3)
