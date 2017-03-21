9ad73c64a9dd51f7592bc393f372f72e580b6cd3
Peter Ivanics <peter@choicely.com>
2017-03-21 14:08:42 +0200

    Added answers to task 4

---
+++ Advanced course in Machine Learning/AML-Week1/gradients.R
@@ -0,0 +1,22 @@
+sigmoid = function(x) {
+  return(1 / (1 + exp(-1 * x)))
+}
+
+sigmoidFirstDerivate = function(x) {
+  return(-exp(x) / (exp(x) + 1)^2)
+}
+
+sigmoidSecondDerivate = function(x) {
+  return(exp(x) * (exp(x) - 1) / (exp(x) + 1)^3)  
+}
+
+logloss = function(data) {
+  return(- data$y * log(sigmoid(t(data$theta) * data$x) - (1 - data$y) * log(1 - sigmoid(t(data$theta) * data$x))))
+}
+
+theta = c(1, 1)
+x = c(-1, 2)
+y = 1
+
+data = data.frame(x, y, theta)
+gradient = data$x * (sigmoid(t(data$theta) - y))
