9849b977a770f15e6f80e5687466e6368713b3ce
Peter Ivanics <peter@choicely.com>
2017-09-20 23:44:15 +0300

    Renaming

--- Introduction to Data Science/week2_exercise4.py
+++ Introduction to Data Science/week2_exercise4.py
@@ -6,8 +6,7 @@ cities = gpd.read_file("data/cities")
 #world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 #cities = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
 
-world = world.to_crs({'init': 'epsg:4326'})
+world = world.to_crs(cities.crs)
 
 base_plot = world.plot(color='white', edgecolor='black')
 cities.plot(ax=base_plot, marker='o', color='red', markersize=5)
-
