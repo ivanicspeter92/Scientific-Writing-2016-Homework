54bb892d9bcdf14fd692c8cc491a0e8344178d31
Peter Ivanics <peter@choicely.com>
2017-09-29 10:03:27 +0300

    Small modifications

--- Introduction to Data Science/TwoTablesJoin/src/main/java/TwoTablesJoin.java
+++ Introduction to Data Science/TwoTablesJoin/src/main/java/TwoTablesJoin.java
@@ -48,12 +48,13 @@ public class TwoTablesJoin {
         }
     }
 
-    public static class TableJoinReducer extends Reducer<Text, ObjectWritable, Text, ObjectWritable> {
+    public static class TableJoinReducer extends Reducer<Text, ObjectWritable, Text, Text> {
         private Text result = new Text();
 
         public void reduce(Text key, Iterable<ObjectWritable> values, Context context) throws IOException, InterruptedException {
             String combinedTables = "";
 
+            System.out.println("Reduce()");
             //context.write(key, combinedTables);
         }
     }
@@ -66,8 +67,10 @@ public class TwoTablesJoin {
         job.setCombinerClass(TableJoinReducer.class);
         job.setReducerClass(TableJoinReducer.class);
 
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(ObjectWritable.class);
         job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(ObjectWritable.class);
+        job.setOutputValueClass(Text.class);
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
@@ -75,22 +78,24 @@ public class TwoTablesJoin {
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
-    public static class Student extends Object {
+    public static class Student extends ObjectWritable {
         String id, name;
         int year;
 
         public Student(String id, String name, int year) {
+            super();
             this.id = id;
             this.name = name;
             this.year = year;
         }
     }
 
-    public static class Score extends Object {
+    public static class Score extends ObjectWritable {
         String studentID;
         int score1, score2, score3;
 
         public Score(String studentID, int score1, int score2, int score3) {
+            super();
             this.studentID = studentID;
             this.score1 = score1;
             this.score2 = score2;
